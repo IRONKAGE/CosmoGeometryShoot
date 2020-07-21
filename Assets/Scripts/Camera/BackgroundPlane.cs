f3b476880453f5c8f9475fc600cb31497f108d12
IRONKAGE <IRONKAGE@gmail.com>
2020-07-21 20:48:20 +0300

    Add Cube fot Target

---
+++ Assets/Scripts/Camera/BackgroundPlane.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class BackgroundPlane : MonoBehaviour {
+
+	private bool mBackgroundWasSwitchedOff = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (!mBackgroundWasSwitchedOff) {
+			BackgroundPlaneBehaviour bgPlane = GetComponent<BackgroundPlaneBehaviour> ();
+			if (bgPlane.enabled) {
+				// switch it off
+				bgPlane.enabled = false;
+			}
+			mBackgroundWasSwitchedOff = true;
+		}
+	}
+}
