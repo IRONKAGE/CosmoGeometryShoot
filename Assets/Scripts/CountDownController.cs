6dc054ee3c9c8cf4c8266bae233e4284874b5bb1
IRONKAGE <IRONKAGE@gmail.com>
2020-07-22 15:58:43 +0300

    add CountDownController to Strart!

---
+++ Assets/Scripts/CountDownController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownController : MonoBehaviour
+{
+    public int countDownTime;
+    public Text countDownDisplay;
+    private void Start()
+    {
+        StartCoroutine(CountDownToStart());
+    }
+
+    IEnumerator CountDownToStart()
+    {
+        while(countDownTime > 0)
+        {
+            countDownDisplay.text = countDownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countDownTime--;
+        }
+
+        countDownDisplay.text = "START!";
+        // GameController.instance.BeginGame();
+        yield return new WaitForSeconds(1f);
+        countDownDisplay.gameObject.SetActive(false);
+    }
+}
